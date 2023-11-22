fa5fe0b5c12501fbff2d1b646f3c27568b7fc8cb
Seongbin Kim <seongbin9786@gmail.com>
2023-11-22 12:19:04 +0900

    fix: 누락했던 공개 API 추가

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import { VDOM } from './vdom/VDOM.js';
 
 export { Component } from './components/Component.js';
 
+// 디버그 메시지 출력이 필요할 때 사용한다.
+export { enableDebugModule } from './debug/debug.js';
+
+// tagged template을 반환할 때 사용한다.
+export { createDOMSpec as jsx } from './parsers/taggedtemplates/Parser.js';
+
 // 컴포넌트를 등록한다.
 export const registerComponent = (name, constructor) => {
     VDOM.componentMap.set(name, constructor);
