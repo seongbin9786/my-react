c92e192133f50f2581d77df52d96d78e387a67b1
Seongbin Kim <seongbin9786@gmail.com>
2023-11-28 19:38:29 +0900

    restructuring: Parser에서 Tokenizer를 분리 독립

---
+++ src/parsers/taggedtemplates/TTJSXTokenizer.js
@@ -0,0 +1,58 @@
+/**
+ * 정규 표현식으로 입력된 (strings, values)를 유의미한 토큰 단위로 분할한다.
+ */
+export class TTJSXTokenizer {
+
+    /**
+     * 테스트 코드 등 직접 (strings, values)를 tagged template에서 가져와야 할 때 사용하는 헬퍼
+     * 
+     * @param {string[]} strings 
+     * @param  {any[]} values 
+     */
+    static raw(strings, ...values) {
+        return { strings, values };
+    }
+
+    /**
+     * @param {string[]} strings 
+     * @param {any[]} values 
+     */
+    tokenize(strings, values) {
+        
+        const mappedStrings = strings.map((string) => 
+            string
+                // 1. 단순 개행 제거
+                .replace(/\r|\n|\t/g, "") 
+
+                // 2. 쉽게 분할 가능한 요소들 분할
+                // split 대상을 (,) 로 감싸 split 이후에도 남게 했음.
+                // "[^"]*" --> "문자열" 매칭 (문자열에서 "는 제외)
+                // <[^\s>\/]* --> <문자열 매칭 (공백과 >는 제외)
+                .split(/("[^"]*"|<[^\s>\/]*|>|\/)/g)
+                
+                // 3. ", = 제거
+                // e.g. "Modal__image" src="hello world"
+                .map((string) => string.split(/"|=/g))
+                .flat()
+
+                // 4. <와 tagName의 분리 필요
+                // <는 제거하지 않고 구분자로써 남김.
+                .map((string) => string.split(/(<)/g))
+                .flat()
+
+                // 5. 최종적으로 빈 공백 문자열 모두 제거
+                .map((string) => string.trim()) // 앞/뒤 약간의 공백 제거
+                .filter((v) => v.length > 0) // 공백만 있던 case 제거
+        );
+
+        const joinedStringsAndValues = mappedStrings.reduce((sum, cur, idx) => {
+            if (idx === 0) {
+                return [ cur ];
+            }
+            
+            return [ ...sum, values[idx - 1], cur ];
+        });
+
+        return joinedStringsAndValues.flat();
+    }
+}
---
+++ tests/TTJSXTokenizer.test.js
@@ -0,0 +1,250 @@
+import { describe, test, expect } from "@jest/globals";
+import { Component, jsx, registerComponent, renderRoot } from "../src";
+import { TTJSXTokenizer } from "../src/parsers/taggedtemplates/TTJSXTokenizer";
+import { DOMSpec } from "../src/spec/DOMSpec";
+
+describe.only("Tokenizer", () => {
+
+    // 테스트에 사용되는 fixture
+    class SimpleComp extends Component {} // 토크나이저가 Component 인스턴스 여부를 확인하지는 않음.
+    const childObject = new DOMSpec("div", {}); // 토크나이저가 DOMSpec 인스턴스 여부를 확인하지는 않음. 
+    
+    test.each([
+        [
+            "type [string], props [no], children [no]",
+            TTJSXTokenizer.raw`<div></div>`,
+            [
+                "<", "div", ">",
+                "<", "/", "div", ">"
+            ]
+        ],
+        [
+            "type [string], props [no], children [string]",
+            TTJSXTokenizer.raw`
+                <div>
+                    <div>
+                        <img className="Modal__image" src="hello world" />
+                    </div>
+                </div>
+            `,
+            [
+                "<", "div", ">",
+                "<", "div", ">",
+                "<", "img", "className", "Modal__image", "src", "hello world", "/", ">",
+                "<", "/", "div", ">",
+                "<", "/", "div", ">"
+            ]
+        ],
+        [
+            "type [string], props [no], children [expr]",
+            TTJSXTokenizer.raw`
+                <div>
+                    ${childObject}
+                </div>
+            `,
+            [
+                "<", "div", ">",
+                childObject,
+                "<", "/", "div", ">"
+            ]
+        ],
+        [
+            "type [string], props [string], children [no]",
+            TTJSXTokenizer.raw`<div hello="world" hello="world" hello="world"></div>`,
+            [
+                "<", "div", "hello", "world", "hello", "world", "hello", "world", ">",
+                "<", "/", "div", ">"
+            ]
+        ],
+        [
+            "type [string], props [string], children [string]",
+            TTJSXTokenizer.raw`
+                <div hello="world" hello="world" hello="world">
+                    <div>
+                        <img className="Modal__image" src="hello world" />
+                    </div>
+                </div>
+            `,
+            [
+                "<", "div", "hello", "world", "hello", "world", "hello", "world", ">",
+                "<", "div", ">",
+                "<", "img", "className", "Modal__image", "src", "hello world", "/", ">",
+                "<", "/", "div", ">",
+                "<", "/", "div", ">"
+            ]
+        ],
+        [
+            "type [string], props [string], children [expr]",
+            TTJSXTokenizer.raw`
+                <div hello="world" hello="world" hello="world">
+                    ${childObject}
+                </div>
+            `,
+            [
+                "<", "div", "hello", "world", "hello", "world", "hello", "world", ">",
+                childObject,
+                "<", "/", "div", ">"
+            ]
+        ],
+        [
+            "type [string], props [expr], children [no]",
+            TTJSXTokenizer.raw`<div hello="world" test=${1} />`,
+            [
+                "<", "div", "hello", "world", "test", 1, "/", ">"
+            ]
+        ],
+        [
+            "type [string], props [expr], children [string]",
+            TTJSXTokenizer.raw`
+                <div hello="world" test=${1}>
+                    <div>
+                        <img className="Modal__image" src="hello world" />
+                    </div>
+                </div>
+            `,
+            [
+                "<", "div", "hello", "world", "test", 1, ">",
+                "<", "div", ">",
+                "<", "img", "className", "Modal__image", "src", "hello world", "/", ">",
+                "<", "/", "div", ">",
+                "<", "/", "div", ">"
+            ]
+        ],
+        [
+            "type [string], props [expr], children [expr]",
+            TTJSXTokenizer.raw`
+                <div hello="world" test=${1}>
+                    ${childObject}
+                </div>
+            `,
+            [
+                "<", "div", "hello", "world", "test", 1, ">",
+                childObject,
+                "<", "/", "div", ">"
+            ]
+        ],
+        [
+            "type [Component], props [no], children [no]",
+            TTJSXTokenizer.raw`<${SimpleComp}></${SimpleComp}>`,
+            [
+                "<", SimpleComp, ">",
+                "<", "/", SimpleComp, ">"
+            ]
+        ],
+        [
+            "type [Component], props [no], children [string]",
+            TTJSXTokenizer.raw`
+                <${SimpleComp}>
+                    <div>
+                        <img className="Modal__image" src="hello world" />
+                    </div>
+                </${SimpleComp}>
+            `,
+            [
+                "<", SimpleComp, ">",
+                "<", "div", ">",
+                "<", "img", "className", "Modal__image", "src", "hello world", "/", ">",
+                "<", "/", "div", ">",
+                "<", "/", SimpleComp, ">"
+            ]
+        ],
+        [
+            "type [Component], props [no], children [expr]",
+            TTJSXTokenizer.raw`
+                <${SimpleComp}>
+                    ${childObject}
+                </${SimpleComp}>
+            `,
+            [
+                "<", SimpleComp, ">",
+                childObject,
+                "<", "/", SimpleComp, ">"
+            ]
+        ],
+        [
+            "type [Component], props [string], children [no]",
+            TTJSXTokenizer.raw`
+                <${SimpleComp} hello="world" hello="world" hello="world" />
+            `,
+            [
+                "<", SimpleComp, "hello", "world", "hello", "world", "hello", "world", "/", ">",
+            ]
+        ],
+        [
+            "type [Component], props [string], children [string]",
+            TTJSXTokenizer.raw`
+                <${SimpleComp} hello="world" hello="world" hello="world">
+                    <div>
+                        <img className="Modal__image" src="hello world" />
+                    </div>
+                </${SimpleComp}>
+            `,
+            [
+                "<", SimpleComp, "hello", "world", "hello", "world", "hello", "world", ">",
+                "<", "div", ">",
+                "<", "img", "className", "Modal__image", "src", "hello world", "/", ">",
+                "<", "/", "div", ">",
+                "<", "/", SimpleComp, ">"
+            ]
+        ],
+        [
+            "type [Component], props [string], children [expr]",
+            TTJSXTokenizer.raw`
+                <${SimpleComp} hello="world" hello="world" hello="world">
+                    ${childObject}
+                </${SimpleComp}>
+            `,
+            [
+                "<", SimpleComp, "hello", "world", "hello", "world", "hello", "world", ">",
+                childObject,
+                "<", "/", SimpleComp, ">"
+            ]
+        ],
+        [
+            "type [Component], props [expr], children [no]",
+            TTJSXTokenizer.raw`<${SimpleComp} hello="world" test=${1} />`,
+            [
+                "<", SimpleComp, "hello", "world", "test", 1, "/", ">"
+            ]
+        ],
+        [
+            "type [Component], props [expr], children [string]",
+            TTJSXTokenizer.raw`
+                <${SimpleComp} hello="world" test=${1}>
+                    <div>
+                        <img className="Modal__image" src="hello world" />
+                    </div>
+                </${SimpleComp}>
+            `,
+            [
+                "<", SimpleComp, "hello", "world", "test", 1, ">",
+                "<", "div", ">",
+                "<", "img", "className", "Modal__image", "src", "hello world", "/", ">",
+                "<", "/", "div", ">",
+                "<", "/", SimpleComp, ">"
+            ]
+        ],
+        [
+            "type [Component], props [expr], children [expr]",
+            TTJSXTokenizer.raw`
+                <${SimpleComp} hello="world" test=${1}>
+                    ${childObject}
+                </${SimpleComp}>
+            `,
+            [
+                "<", SimpleComp, "hello", "world", "test", 1, ">",
+                childObject,
+                "<", "/", SimpleComp, ">"
+            ]
+        ],
+    ])("%s", (_title, { strings, values }, expected) => {
+
+        // when
+        const parser = new TTJSXTokenizer();
+        const result = parser.tokenize(strings, values);
+
+        // then
+        expect(result).toEqual(expected);
+    });
+    
+});
